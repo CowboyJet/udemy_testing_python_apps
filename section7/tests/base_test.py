56f11c8e43a86ad578c27a2f77af3a3023f9a578
Robert van der Dussen <robert.van.der.dussen@ing.com>
2023-10-24 09:30:58 +0200

    updated base test to allow for full testsuite run

--- section7/tests/base_test.py
+++ section7/tests/base_test.py
@@ -19,8 +19,9 @@ class BaseTest(TestCase):
     def setUpClass(cls):
         # Make sure database exists
         app.config['SQLALCHEMY_DATABASE_URI'] = BaseTest.SQLALCHEMY_DATABASE_URI
-        with app.app_context():
-            db.init_app(app)
+        if not "sqlalchemy" in app.extensions:
+            with app.app_context():
+                db.init_app(app)
 
     def setUp(self):
         with app.app_context():
